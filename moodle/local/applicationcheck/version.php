fb1ed5b6dfbeeda9e3f9754279fbfacfd0dff0c3
ankurproj <155262565+ankurproj@users.noreply.github.com>
2026-01-17 03:03:52 +0530

    Initial Commit

---
+++ moodle/local/applicationcheck/version.php
@@ -0,0 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'tool_applicationcheck';
+$plugin->version   = 2025060900;       
+$plugin->requires  = 2024042200;       
+$plugin->maturity  = MATURITY_ALPHA;
+$plugin->release   = '0.1';
